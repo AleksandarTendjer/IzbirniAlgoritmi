080a65bc2d5f2c0b70e7175c33a3557bb7dcbe05
AleksandarTendjer <tendjer@protonmail.com>
2019-10-14 15:00:11 +0200

    Timer added but not showing improvement

--- IzbraniAlgVaja1/Volitve.cpp
+++ IzbraniAlgVaja1/Volitve.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <iostream>
+#include <ctime>
 #include "RandomGenerator.h"
 #include "PrimeRandomNumberGenerator.h"
 #include "Naivna.h"
@@ -8,32 +9,40 @@ using namespace std;
 
 int main()
 {
+
 	//RandomGenerator* Random = new RandomGenerator();
 	int userInput;
-	cout << "Izbira opcija: \n\r 1)SuperDuper Generator \n\r 2) Naivna \n\r 3) Miler-Rabin \n\r";
-	cin >>userInput ;
-	long a;
-	int i = 0;
 	RandomGenerator Random;
-	long p = Random.LCG();
-	long long * result;
+	long  long p;
+	double duration;
 	PrimeRandomNumberGenerator PrimeGenerator;
+	std::clock_t start;
+	
+	cout << "Choose an option: \n\r 1)SuperDuper Generator \n\r 2) Naive \n\r 3) Miler-Rabin \n\r";
+	cin >>userInput ;
+
+
 	switch (userInput)
 	{
 	
 	case(1):
 	
-		a=Random.LCG();
-		cout <<"Resitva je :"<< a;
+		p=Random.LCG();
+		cout <<"Resitva je :"<< p;
 		break; 
 	case(2):
 		cout << "-------Naive Generator-----\r\n";
-			  cout<<PrimeGenerator.Naive()<<"\r\n";
-			  cout << "-------Naive Tester-----\r\n";
+
+		cout<<PrimeGenerator.Naive()<<"\r\n";
+
+		cout << "-------Naive Tester-----\r\n";
 			  //ispitati za 19 i izmeriti vreme
 			  cout << "Enter a number:\r\n";
 			  cin >> p;
+			  start = std::clock();
 			  cout << " \n\r " << p << " is: " << (PrimeGenerator.NaiveTest(p) ? "a prime number\r\n" : "not a prime number \r\n");
+			  duration = (double)(std::clock() - (double)start) / (double)CLOCKS_PER_SEC;
+			  cout << duration;//duration of fnction
 			  /*while (*(result+i)<61169)
 			{
 				cout << " \n\r" << *(result+i);
@@ -41,14 +50,18 @@ int main()
 			}*/
 			break;
 	case(3):
+
 		cout << "-------MilerRabin Generator-----\r\n";
 		cout<<PrimeGenerator.MilerRabin()<<"\r\n";
+
 		//ispitati za 19 i zimeriti vreme
 		cout << "-------MilerRabin Tester-------\r\n";
 		cout << "Enter a number: \r\n";
 		cin >> p;
+		start = std::clock();
 		cout << " \n\r "<<p<<" is: "<< (PrimeGenerator.MilerRabinTest(p,3)?"a prime number\r\n":"not a prime number \r\n");
-		
+		duration = (double)(std::clock() - (double)start) / (double)CLOCKS_PER_SEC;
+		cout << duration;//duration of function
 		
 		break;
 	default:
